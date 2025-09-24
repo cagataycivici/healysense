6fe83e9cef5cb29914897f9b87930091956c0361
Atakan <tepe_atakan@outlook.com>
2025-09-24 10:43:31 +0300

    cleanup

--- components/pages/logistic/Footer.tsx
+++
@@ -1,8 +0,0 @@
-import Footer from '@/components/Footer';
-import React from 'react';
-
-const LogisticFooter = () => {
-    return <Footer className="mt-64" image="/pages/logistic/footer-image.jpg" />;
-};
-
-export default LogisticFooter;
--- components/pages/real-estate/Footer.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Footer from '@/components/Footer';
-const RealEstateFooter = () => {
-    return <Footer className="mt-64" image="/pages/real-estate/footer-image.jpg" />;
-};
-
-export default RealEstateFooter;
