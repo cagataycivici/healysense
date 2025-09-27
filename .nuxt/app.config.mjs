d3c08ab9ddbefef56a828df2a6791fef5c513d95
Cagatay Civici <cagatay.civici@gmail.com>
2025-09-27 13:16:01 +0300

    New website

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/Users/jagatai/Development/projects/hsense/healysense/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default /* #__PURE__ */ defuFn(inlineConfig)
---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///Users/jagatai/Development/projects/hsense/healysense/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
